6d1a5fe6a3f661af2a88cd0133e01c9b78974b4a
Ramil Ashrafulin <HEHAHA@bk.ru>
2021-10-20 19:21:07 +0600

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styles
+import styles from "./Footer.module.css";
+// Styles
+
+const Footer = () => {
+  return (
+    <header className={styles.footer}>
+      <h1 className={styles.title}>Footer</h1>
+    </header>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Styles
+import styles from "./Header.module.css";
+// Styles
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
